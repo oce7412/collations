0ddab4d83d369545d444c4ca4569c81b52b9c15f
Océane MAHE <oceane.mahe5@etu.univ-lorraine.fr>
2025-06-10 15:36:45 +0200

    Fix Render config 2

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
 @app.route("/")
 def home():
-    return "Bienvenue sur l'app Flask"
+    return "Bienvenue sur lapp Flask"
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
